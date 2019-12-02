0d1f4accf9952c5e26ae5939b1ecf92d92130637
jsills3110 <jsills3110@gmail.com>
2019-12-01 18:31:14 -0800

    Added SQL schema and seeds.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut Butter Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Donut Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Onion Burger', false);
