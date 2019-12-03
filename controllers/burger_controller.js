60ef95a1b0f775e82dd5b67b913372c2a2d15609
jsills3110 <jsills3110@gmail.com>
2019-12-02 20:31:34 -0800

    Completed burger_controller.js which handles the routes of the app.

---
+++ controllers/burger_controller.js
@@ -0,0 +1,58 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function(data) {
+        var obj = {
+            burgers: data
+        };
+        console.log(obj);
+        res.render("index", obj);
+    });
+});
+
+router.post("/api/burgers", function(req,res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("Condition: ", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req,res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("Condition: ", condition);
+
+    burger.delete(condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
